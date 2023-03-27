f57ba4bc6f647ffa86bb8a6bca8edf419403eaf6
SergioPanov <www.vapanov@gmail.com>
2023-03-27 20:19:13 +0200

    hw-js-corrected

--- js/task-01.js
+++ js/task-01.js
@@ -1,12 +1,8 @@
 const categoriesList = document.querySelector("#categories");
-const categories = categoriesList.querySelectorAll(".item");
 
-console.log(`Number of categories: ${categories.length}`);
+console.log(`Number of categories: ${[...categoriesList.children].length}`);
 
-categories.forEach((category) => {
-  const categoryName = category.querySelector("h2").textContent;
-  const categoryItems = category.querySelectorAll("li").length;
-
-  console.log(`Category: ${categoryName}`);
-  console.log(`Elements: ${categoryItems}`);
+[...categoriesList.children].forEach((el) => {
+  console.log(`Category : ${el.querySelector("h2").textContent}`);
+  console.log(`Elements : ${el.querySelectorAll("li").length}`);
 });
